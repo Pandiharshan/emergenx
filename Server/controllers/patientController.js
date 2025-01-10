26242ec48d09e30cb6044d6605cacfca803c7d63
PandiHarshan <kit27.am35@gmail.com>
2025-01-10 05:38:26 +0000

    docs(database): enhance database triage system

--- Server/controllers/patientController.js
+++ Server/controllers/patientController.js
@@ -1,3 +1,5 @@
-﻿// Updated: 2025-12-19 12:03:31 - test(api): refactor triage system in api (fixes #20)
+﻿// Updated: 2025-12-19 12:03:33 - fix: implement socket authentication
+// Updated: 2025-12-19 12:03:31 - test(api): refactor triage system in api (fixes #20)
 // Auto-generated file for EmergenX
+
 
