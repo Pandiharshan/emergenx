9b16be9d11ee836ca82f7126ce3434bc14897195
PandiHarshan <kit27.am35@gmail.com>
2025-01-22 05:56:54 +0000

    chore(auth): update auth patient form

--- Server/controllers/patientController.js
+++ Server/controllers/patientController.js
@@ -1,7 +1,9 @@
-﻿// Updated: 2025-12-19 12:03:35 - test(ui): improve database schema in ui (fixes #16)
+﻿// Updated: 2025-12-19 12:03:59 - docs: update auth patient form
+// Updated: 2025-12-19 12:03:35 - test(ui): improve database schema in ui (fixes #16)
 // Updated: 2025-12-19 12:03:33 - fix: implement socket authentication
 // Updated: 2025-12-19 12:03:31 - test(api): refactor triage system in api (fixes #20)
 // Auto-generated file for EmergenX
+
 
 
 
