7e482ce8fea0fc98916d08b3e082fc9020ce233b
PandiHarshan <kit27.am35@gmail.com>
2025-02-02 07:37:07 +0000

    perf(database): update database error handling

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,4 +1,5 @@
-﻿// Updated: 2025-12-19 13:14:06 - style(patient): improve patient form (fixes #16)
+﻿// Updated: 2025-12-19 13:14:10 - refactor: implement ui UI components
+// Updated: 2025-12-19 13:14:06 - style(patient): improve patient form (fixes #16)
 // Updated: 2025-12-19 13:14:01 - perf(triage): update error handling in triage (fixes #27)
 // Updated: 2025-12-19 12:27:57 - perf: update patient authentication
 // Updated: 2025-12-19 12:27:50 - refactor(auth): enhance auth dashboard
@@ -224,6 +225,7 @@ const loginUser = async (req, res) => {
 };
 
 export { registerUser, loginUser };
+
 
 
 
