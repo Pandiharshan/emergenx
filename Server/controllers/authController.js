189885cd21383216e1508b60b2f7f27fadea8708
PandiHarshan <kit27.am35@gmail.com>
2025-02-26 03:31:25 +0000

    refactor(api): optimize UI components

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,4 +1,5 @@
-﻿// Updated: 2025-12-19 12:05:00 - chore(api): enhance UI components
+﻿// Updated: 2025-12-19 12:05:00 - style(patient): refactor patient database schema
+// Updated: 2025-12-19 12:05:00 - chore(api): enhance UI components
 // Updated: 2025-12-19 12:04:56 - docs(triage): update error handling in triage
 // Updated: 2025-12-19 12:04:51 - feat: fix ui API endpoints
 // Updated: 2025-12-19 12:04:40 - fix(auth): implement authentication (fixes #29)
@@ -147,6 +148,7 @@ const loginUser = async (req, res) => {
 };
 
 export { registerUser, loginUser };
+
 
 
 
