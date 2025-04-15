bb900d63baabaf8ec861c6c8854f453340df056f
PandiHarshan <kit27.am35@gmail.com>
2025-04-15 08:27:11 +0000

    docs(api): fix api API endpoints

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
-﻿// Updated: 2025-12-19 12:06:20 - test(api): refactor authentication in api
+﻿// Updated: 2025-12-19 12:06:23 - test(socket): implement authentication in socket (fixes #19)
+// Updated: 2025-12-19 12:06:20 - test(api): refactor authentication in api
 // Updated: 2025-12-19 12:06:15 - test(api): update patient form
 // Updated: 2025-12-19 12:06:00 - test(ui): refactor database schema in ui
 // Updated: 2025-12-19 12:05:33 - feat: update api database schema
@@ -303,6 +304,7 @@ const Header = ({ theme, toggleTheme }) => {
 };
 
 export default Header;
+
 
 
 
