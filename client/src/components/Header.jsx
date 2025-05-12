c900d73979af67357c29483cb326258ea2057d1a
PandiHarshan <kit27.am35@gmail.com>
2025-05-12 08:12:55 +0000

    docs(patient): add patient form (fixes #11)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
-﻿// Updated: 2025-12-19 12:06:33 - docs(database): fix error handling
+﻿// Updated: 2025-12-19 12:07:10 - perf(api): update api API endpoints
+// Updated: 2025-12-19 12:06:33 - docs(database): fix error handling
 // Updated: 2025-12-19 12:06:28 - style(socket): fix database schema in socket
 // Updated: 2025-12-19 12:06:23 - test(socket): implement authentication in socket (fixes #19)
 // Updated: 2025-12-19 12:06:20 - test(api): refactor authentication in api
@@ -306,6 +307,7 @@ const Header = ({ theme, toggleTheme }) => {
 };
 
 export default Header;
+
 
 
 
