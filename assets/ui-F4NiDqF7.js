71116d1926903d8736cde636cb47b5e4ba8dfbb8
Pandi Harshan <kit27.am35@gmail.com>
2025-08-02 13:54:26 +0530

    Updates

---
+++ assets/ui-F4NiDqF7.js
@@ -0,0 +1,21 @@
+import{r as o}from"./vendor-Gm9i_4Ku.js";/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var l={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=t=>t.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase().trim(),s=(t,c)=>{const a=o.forwardRef(({color:p="currentColor",size:e=24,strokeWidth:n=2,absoluteStrokeWidth:h,className:d="",children:r,...m},i)=>o.createElement("svg",{ref:i,...l,width:e,height:e,stroke:p,strokeWidth:h?Number(n)*24/Number(e):n,className:["lucide",`lucide-${y(t)}`,d].join(" "),...m},[...c.map(([k,u])=>o.createElement(k,u)),...Array.isArray(r)?r:[r]]));return a.displayName=`${t}`,a};/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=s("Moon",[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]]);/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=s("Sun",[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]]);export{g as M,M as S};
